99e27d53f7d748706a26ac246ecb03638084a7ab
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2018-01-22 10:50:42 -0600

    - Removed the SplineCurve.cs script since it is now obsolete and unused - Modified BezierSplineInspector.cs so that we can toggle if we want to show the point handle transforms or the curve velocity lines for ease-of-use. I also added in some comments that I left out last night while I was too tired to think much - Modified the BezierSpline.cs script's GetPoint and GetVelocity functions so that we can find the correct points and velocities now that we're working with multiple splines stitched together

--- Starlight/Assets/Editor/BezierSplineInspector.cs
+++ Starlight/Assets/Editor/BezierSplineInspector.cs
@@ -6,8 +6,13 @@ using UnityEditor;
 [CustomEditor(typeof(BezierSpline))]
 public class BezierSplineInspector : Editor
 {
-    //The number of steps between each curve segment
-    private const int lineSteps = 10;
+    //Bool that determines if we display the transform handles for each spline point
+    private bool showHandles = true;
+    //Bool that determines if we display the lines for the spline curve velocity
+    private bool showVelocityLines = true;
+
+    //The number of steps along each curve
+    private const int stepsPerCurve = 10;
 
     //Reference to the selected curve
     private BezierSpline spline;
@@ -47,29 +52,43 @@ public class BezierSplineInspector : Editor
             Handles.DrawLine(p2, p3);
 
             //Drawing the bezier curve
-            Handles.DrawBezier(p0, p3, p1, p2, this.spline.splineColor, null, 2f);
+            Handles.DrawBezier(p0, p3, p1, p2, this.spline.splineColor, null, this.spline.splineWidth);
 
             //Setting it so that the last point on this drawn curve is the starting point for the next curve
             p0 = p3;
         }
 
-        this.ShowDirections();
+        //If we show the velocity lines, we draw them
+        if (this.showVelocityLines)
+        {
+            this.ShowDirections();
+        }
     }
 
 
     //Function called from the editor window
     public override void OnInspectorGUI()
     {
+        //Calling the base function
         base.OnInspectorGUI();
 
+        //Getting the selected spline reference
         this.spline = target as BezierSpline;
+
         //Creating a GUI button to add a curve to our spline
         if(GUILayout.Button("Add Curve"))
         {
+            //Sets the spline to dirty so that we can save changes
             Undo.RecordObject(this.spline, "Add Curve");
+            //Adds new points to the end of our spline
             this.spline.AddCurve();
             EditorUtility.SetDirty(this.spline);
         }
+
+        //Creating a GUI toggle for if we should show the transform handles for points
+        this.showHandles = GUILayout.Toggle(this.showHandles, "Show Handles");
+        //Creating a GUI toggle for if we should show the velocity lines for the curves
+        this.showVelocityLines = GUILayout.Toggle(this.showVelocityLines, "Show Velocity");
     }
 
 
@@ -79,31 +98,42 @@ public class BezierSplineInspector : Editor
         //The position of the point we return
         Vector3 point = this.handleTransform.TransformPoint(this.spline.points[index_]);
 
-        //Checking to see if we're trying to move the handle for the given point
-        EditorGUI.BeginChangeCheck();
-        point = Handles.DoPositionHandle(point, handleRotation);
-        //If we're done changing the point handle, we apply the change to the curve's point
-        if (EditorGUI.EndChangeCheck())
+        //If we show this spline's handles or not
+        if (this.showHandles)
         {
-            Undo.RecordObject(this.spline, "Move Point");
-            EditorUtility.SetDirty(this.spline);
-            this.spline.points[index_] = this.handleTransform.InverseTransformPoint(point);
+            //Checking to see if we're trying to move the handle for the given point
+            EditorGUI.BeginChangeCheck();
+            point = Handles.DoPositionHandle(point, handleRotation);
+
+            //If we're done changing the point handle, we apply the change to the curve's point
+            if (EditorGUI.EndChangeCheck())
+            {
+                //Sets the spline to dirty so that we can save changes
+                Undo.RecordObject(this.spline, "Move Point");
+                EditorUtility.SetDirty(this.spline);
+                //Moves the point at the given index to the handle transform's position
+                this.spline.points[index_] = this.handleTransform.InverseTransformPoint(point);
+            }
         }
 
         return point;
     }
 
 
-    //Function called from OnSceneGUI
+    //Function called from OnSceneGUI to draw the velocity lines along the curves
     private void ShowDirections()
     {
+        //Drawing the velocity lines for each point
         Handles.color = this.spline.velocityLineColor;
         Vector3 point = this.spline.GetPoint(0f);
         Handles.DrawLine(point, point + this.spline.GetDirection(0f) * directionScale);
-        for (int i = 1; i <= lineSteps; i++)
+
+        //Looping through each step in the curve
+        int steps = stepsPerCurve * this.spline.CurveCount;
+        for (int i = 1; i <= steps; i++)
         {
-            point = this.spline.GetPoint(i / (float)lineSteps);
-            Handles.DrawLine(point, point + this.spline.GetDirection(i / (float)lineSteps * directionScale));
+            point = this.spline.GetPoint(i / (float)steps);
+            Handles.DrawLine(point, point + this.spline.GetDirection(i / (float)steps * directionScale));
         }
     }
 }
--- Starlight/Assets/Scripts/Gameplay/Math/Curves/BezierSpline.cs
+++ Starlight/Assets/Scripts/Gameplay/Math/Curves/BezierSpline.cs
@@ -6,6 +6,7 @@ public class BezierSpline : MonoBehaviour
 {
     //Color for the spline
     public Color splineColor = Color.red;
+    public float splineWidth = 5;
 
     //Color for the line connecting the handles
     public Color handleLineColor = Color.green;
@@ -16,6 +17,7 @@ public class BezierSpline : MonoBehaviour
     //The list of control points that make up this spline
     public List<Vector3> points;
 
+    //Int that holds the number of bezier curves along this spline
     public int CurveCount
     {
         get
@@ -39,18 +41,67 @@ public class BezierSpline : MonoBehaviour
     }
 
 
-    //Function called externally from BezierSplineInspector.cs to access a specific handle point on our spline
+    //Function called externally from BezierSplineInspector.cs to access a specific handle point on our spline based on the percentage of the way through the spline (t_)
     public Vector3 GetPoint(float t_)
     {
-        .//About halfway through the tutorial. Stopped here just after the CurveCount int
-        return this.transform.TransformPoint(Bezier.GetPoint(this.points[0], this.points[1], this.points[2], this.points[3], t_));
+        //Int to hold the index of the starting point of the curve we're going to find the point along
+        int i;
+
+        //If the percent along the spline is above 100%, we find the starting point of the last curve
+        if(t_ >= 1f)
+        {
+            t_ = 1f;
+            i = this.points.Count - 4;
+        }
+        //Otherwise we find the starting point of the curve that's at the percent given
+        else
+        {
+            //Finding which curve is at the percent
+            t_ = Mathf.Clamp01(t_) * this.CurveCount;
+            i = (int)t_;
+            //Finding the index of the first point along that curve
+            t_ -= i;
+            i *= 3;
+        }
+
+        //Getting the point along our curve at the given percent
+        return this.transform.TransformPoint(Bezier.GetPoint(this.points[i], 
+                                                             this.points[i + 1], 
+                                                             this.points[i + 2], 
+                                                             this.points[3], 
+                                                             t_));
     }
 
 
     //Function called externally from BezierSplineInspector.cs to get the tangent lines to the spline
     public Vector3 GetVelocity(float t_)
     {
-        return this.transform.TransformPoint(Bezier.GetFirstDerivative(this.points[0], this.points[1], this.points[2], this.points[3], t_)) - this.transform.position;
+        //Int to hold the index of the starting point of the curve we're going to find the point along
+        int i;
+
+        //If the percent along the spline is above 100%, we find the starting point of the last curve
+        if (t_ >= 1f)
+        {
+            t_ = 1f;
+            i = this.points.Count - 4;
+        }
+        //Otherwise we find the starting point of the curve that's at the percent given
+        else
+        {
+            //Finding which curve is at the percent
+            t_ = Mathf.Clamp01(t_) * this.CurveCount;
+            i = (int)t_;
+            //Finding the index of the first point along that curve
+            t_ -= i;
+            i *= 3;
+        }
+
+        //Getting the rate of change of the bezier at the given percent
+        return this.transform.TransformPoint(Bezier.GetFirstDerivative(this.points[i], 
+                                                                       this.points[i + 1], 
+                                                                       this.points[i + 2], 
+                                                                       this.points[i + 3], 
+                                                                       t_)) - this.transform.position;
     }
 
 
