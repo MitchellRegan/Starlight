042f4b3a1bd8c73547d37b2f1fce9bd9c709da56
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2018-02-04 02:21:59 -0600

    - Made the following objects into prefabs so they can be copied for each level: P1 and P2 Camera Mount to follow the player ships, UI Canvases to hold the different UI screens for single player and co-op. - Moved the player UI canvas objects from the GameData prefab to the new UI Canvases prefab - Modified GlobalData.cs to include variables for which player ships to spawn in gameplay levels and a bool that lets us know if we're loading stuff for single player or co-op - Created the PlayerStartingPosition.cs script to spawn in the correct player ship and set it to have the correct player controls - Created the ActivateCombatUI.cs script to turn on the correct UI for either single player or co-op combat UI

---
+++ Starlight/Assets/Scripts/UI/ActivateCombatUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCombatUI : MonoBehaviour
+{
+    //The game object for the single player UI
+    public GameObject singlePlayerUIObj;
+    //The game object for the co-op UI
+    public GameObject coOpUIObj;
+
+
+
+	// Use this for initialization
+	private void Awake()
+    {
+        //If the game is single player, we only activate the p1 UI
+        if(GlobalData.singlePlayerMode)
+        {
+            this.singlePlayerUIObj.SetActive(true);
+            this.coOpUIObj.SetActive(false);
+        }
+        //If the game is co-op, we activate both p1 and p2 UI
+        else
+        {
+            this.singlePlayerUIObj.SetActive(false);
+            this.coOpUIObj.SetActive(true);
+        }
+    }
+}
